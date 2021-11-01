8ac3c30957ec92378bfcb69f1d01d56730b414ee
unknown <you@example.com>
2021-11-01 22:29:44 +0700

    stripe

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -21,12 +21,14 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
     try {
-        const user = await User.findOne({ username: req.body.username });
+        const user = await User.findOne({ userName: req.body.user_name });
         !user && res.status(401).json("Wrong credentials!");
         const hashedPassword = CryptoJS.AES.decrypt(user.password,process.env.PASS_SEC
     );
-        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-        OriginalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        const inputPassword = req.body.password;
+
+        originalPassword != inputPassword && res.status(401).json("Wrong Password");
         const accessToken = jwt.sign(
             {
                 id: user._id,
